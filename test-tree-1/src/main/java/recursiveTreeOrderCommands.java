80d3b17b1553ed3ecb1c8588b0fc3dc32b2b0ff5
Peter Simov <peter.simov@gmail.com>
2019-11-24 16:20:38 +0200

    SUM001 Invoke Java with task requests. Investigate passing arguments. Internal proof of concept

---
+++ test-tree-1/src/main/java/recursiveTreeOrderCommands.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+//import java.util.List;
+import java.util.LinkedHashMap;
+
+
+public class recursiveTreeOrderCommands {
+	public recursiveTreeOrderCommands(java.util.ArrayList<?> tasks) {
+		System.out.println(tasks);
+		
+		/*Iterator<LinkedHashMap<String, String>> i = commands.iterator();
+	      System.out.println("The ArrayList elements are:");
+	      while (i.hasNext()) {
+	         System.out.println(i.next());
+	      }
+*/
+	}
+	public void init() {
+
+	}
+}
